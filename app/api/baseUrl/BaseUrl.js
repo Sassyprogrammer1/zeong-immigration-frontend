c7d61bd6e9ac698b4f24afa7c2b7052d43afc2bf
Sassyprogrammer1 <hamzajamshed.cs@gmail.com>
2022-12-27 23:22:12 +0500

    baseUrl changed

--- app/api/baseUrl/BaseUrl.js
+++ app/api/baseUrl/BaseUrl.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://xxpgiztvf9.execute-api.us-east-2.amazonaws.com/dev/api/v1',
+  baseURL: 'https://nt9uuakstj.execute-api.us-east-2.amazonaws.com/dev/api/v1',
 });
 
 export default api;
