34db744f182a20e516c684336569b916285d32b5
aliaqib13 <aliaqib13@gmail.com>
2023-02-26 02:19:05 +0500

    location page ui complete

--- app/containers/Pages/consultantFaculty/Location.js
+++ app/containers/Pages/consultantFaculty/Location.js
@@ -1,7 +1,38 @@
 import React from 'react';
+import './styles.css';
 
 const Location = () => (
-  <div>Location</div>
+  <div className='location-container' style={{ backgroundImage: 'url("/assets/geolocation-icons/canadaBackGround.PNG")' }}>
+
+    <div className='mobile-container'>
+      <img src="/assets/geolocation-icons/canada.PNG" alt="" />
+    </div>
+    <div className='image-container' >
+      <div className='image-card' >
+        <img src="/assets/geolocation-icons/west.PNG" alt="" />
+      </div>
+      <div className='image-card'>
+
+      </div>
+      <div className='image-card'>
+        <img src="/assets/geolocation-icons/east.PNG" alt="" />
+
+      </div>
+      <div className='image-card'>
+        <img src="/assets/geolocation-icons/toronto.PNG" alt="" />
+
+      </div>
+      <div className='image-card'>
+        <img src="/assets/geolocation-icons/central.PNG" alt="" />
+
+      </div>
+      <div className='image-card'>
+        <img src="/assets/geolocation-icons/vancouver.PNG" alt="" />
+
+      </div>
+
+    </div>
+  </div>
 );
 
 export default Location;
