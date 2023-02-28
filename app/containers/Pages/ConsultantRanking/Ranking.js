694b361eed41433bedf36b1c2e18b02334f84454
aliaqib13 <aliaqib13@gmail.com>
2023-02-28 22:48:19 +0500

    all the ranking data will set accroding to ranking of university

--- app/containers/Pages/ConsultantRanking/Ranking.js
+++ app/containers/Pages/ConsultantRanking/Ranking.js
@@ -22,8 +22,6 @@ const styles = ({
   }
 });
 const Ranking = (props) => {
-  const { classes } = props;
-
   const history = useHistory();
   const settings = {
     dots: true,
@@ -39,16 +37,41 @@ const Ranking = (props) => {
   };
 
   const computerScience = [
-    { name: 'Alberta', color: ' #339933' },
-    { name: 'Western', color: '#33FF66' },
-    { name: 'Queen', color: ' #99FF99 ' },
-    { name: 'York', color: ' #FFCC66 ' },
-    { name: 'McGill', color: ' #CC9900 ' },
-    { name: 'Ottawa', color: ' #CC9966' },
-    { name: 'UBC', color: ' #CC9999' },
-    { name: 'Toronto', color: ' #CC6600' },
+    { name: 'UBC', color: ' #339933' },
+    { name: 'Toronto', color: '#33FF66' },
+    { name: 'Waterloo', color: ' #99FF99 ' },
+    { name: 'McGill', color: ' #FFCC66 ' },
+    { name: 'Alberta', color: ' #CC9900 ' },
+    { name: 'Montréal', color: ' #CC9966' },
+    { name: 'Simon Fraser', color: ' #CC9999' },
+    { name: 'Ottawa', color: ' #CC6600' },
   ];
 
+  const NursingFuculty = [
+    { name: 'UBC', color: ' #339933' },
+    { name: 'Toronto', color: '#33FF66' },
+    { name: 'Alberta', color: ' #99FF99 ' },
+    { name: 'McGill', color: ' #FFCC66 ' },
+    { name: 'McMaster', color: ' #CC9900 ' },
+    { name: 'Dalhousie', color: ' #CC9966' },
+    { name: 'Queens', color: ' #CC9999' },
+    { name: 'Calgary', color: ' #CC6600' },
+  ];
+  const bussinessFuculty = [
+    { name: 'UBC', color: ' #339933' },
+    { name: 'Toronto', color: '#33FF66' },
+
+    { name: 'McGill', color: ' #99FF99 ' },
+
+    { name: "Queen's", color: ' #FFCC66' },
+
+    { name: 'Western', color: '  #CC9900' },
+
+    { name: 'Alberta', color: ' #CC9966' },
+    { name: 'York', color: ' #CC9999' },
+    { name: 'Calgary', color: ' #CC6600' },
+
+  ];
   const canadaRanking = [
     { name: 'Alberta', color: ' #339933' },
     { name: 'Western', color: '#33FF66' },
@@ -62,16 +85,48 @@ const Ranking = (props) => {
   ];
 
   const facultyEducation = [
-    { name: 'Alberta', color: ' #339933' },
-    { name: 'Western', color: '#33FF66' },
-    { name: 'Queen', color: ' #99FF99 ' },
-    { name: 'York', color: ' #FFCC66 ' },
-    { name: 'McGill', color: ' #CC9900 ' },
-    { name: 'Ottawa', color: ' #CC9966' },
-    { name: 'UBC', color: ' #CC9999' },
-    { name: 'Toronto', color: ' #CC6600' },
+    { name: 'UBC', color: ' #339933' },
+    { name: 'Toronto', color: '#33FF66' },
+    { name: 'Alberta', color: ' #99FF99 ' },
+    { name: 'Western', color: ' #FFCC66 ' },
+    { name: 'Queens', color: ' #CC9900 ' },
+    { name: 'York', color: ' #CC9966' },
+    { name: 'McGill', color: ' #CC9999' },
+    { name: 'Ottawa', color: ' #CC6600' },
   ];
 
+  const engineeringFuculty = [
+    { name: 'UBC', color: ' #339933' },
+    { name: 'Toronto', color: '#33FF66' },
+    { name: 'Waterloo', color: ' #99FF99 ' },
+    { name: 'McGill', color: ' #FFCC66 ' },
+    { name: 'Alberta', color: ' #CC9900 ' },
+    { name: 'McMaster', color: ' #CC9966' },
+    { name: 'Queens', color: ' #CC9999' },
+    { name: 'Montréal', color: ' #CC6600' },
+  ];
+  const pychologyFuculty = [
+    { name: 'UBC', color: ' #339933' },
+    { name: 'McGill', color: '#33FF66' },
+    { name: 'Toronto', color: ' #99FF99 ' },
+    { name: 'Alberta', color: ' #FFCC66 ' },
+    { name: 'Western', color: ' #CC9900 ' },
+    { name: 'York', color: ' #CC9966' },
+    { name: 'Calgary', color: ' #CC9999' },
+    { name: 'Dalhousie', color: ' #CC6600' },
+  ];
+
+  const fucultyNursing = [
+    { name: 'UBC', color: ' #339933' },
+    { name: 'Toronto', color: '#33FF66' },
+    { name: 'Alberta', color: ' #99FF99 ' },
+    { name: 'McGill', color: ' #FFCC66 ' },
+    { name: 'McMaster', color: ' #CC9900 ' },
+    { name: 'Dalhousie', color: ' #CC9966' },
+    { name: 'Queens', color: ' #CC9999' },
+    { name: 'Calgary', color: ' #CC6600' },
+
+  ];
   return (
     <>
       <Button variant="contained" color="primary" onClick={() => history.goBack()}>
@@ -81,12 +136,11 @@ const Ranking = (props) => {
       <div className="container">
         <Slider {...settings}>
           <Tabels tableImg="/assets/university-ranking-icons/canada-ranking.PNG" UniData={canadaRanking} />
-          <Tabels tableImg="/assets/university-ranking-icons/faculty-pychology.PNG" UniData={canadaRanking} />
-          <Tabels tableImg="/assets/university-ranking-icons/faculty-business.PNG" UniData={canadaRanking} />
+          <Tabels tableImg="/assets/university-ranking-icons/faculty-pychology.PNG" UniData={pychologyFuculty} />
+          <Tabels tableImg="/assets/university-ranking-icons/faculty-business.PNG" UniData={bussinessFuculty} />
           <Tabels tableImg="/assets/university-ranking-icons/faculty-education.PNG" UniData={facultyEducation} />
-          <Tabels tableImg="/assets/university-ranking-icons/faculty-engineer.PNG" UniData={canadaRanking} />
-          <Tabels tableImg="/assets/university-ranking-icons/faculty-nursing.PNG" UniData={canadaRanking} />
-
+          <Tabels tableImg="/assets/university-ranking-icons/faculty-engineer.PNG" UniData={engineeringFuculty} />
+          <Tabels tableImg="/assets/university-ranking-icons/faculty-nursing.PNG" UniData={fucultyNursing} />
           <Tabels tableImg="/assets/university-ranking-icons/faculty-science.PNG" UniData={computerScience} />
 
         </Slider>
